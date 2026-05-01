3fdee5f45ddee34b9894b993e00a0105f47b57c3
idriss Tagny <idrisstagny@gmail.com>
2026-05-01 18:48:54 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(),
+     react(),
+     base: process.env.VITE_BASE_URL || "/",
+    
+    ],
 });
